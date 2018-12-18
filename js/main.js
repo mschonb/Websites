ca180d425f3f506a7db5a78ab8064bf9aea07aa5
Marcelo Schonbrunn Bonnin <a01207573@itesm.mx>
2018-12-18 11:01:08 -0600

    Added all files for MiQroTecSis

---
+++ js/main.js
@@ -0,0 +1,47 @@
+$(document).ready(function(){
+
+  //init ScrollMagic
+  var controller = new ScrollMagic.Controller();
+
+  //Scene 01 - pinned
+  var pinScene01 = new ScrollMagic.Scene({
+    triggerElement: '#slide01',
+    triggerHook: 0,
+    duration: '100%'
+
+  })
+  .setPin('#slide01 .pin-wrapper')
+  .addTo(controller)
+  ;
+
+  var pinScene02 = new ScrollMagic.Scene({
+    triggerElement: '#slide01',
+    triggerHook: 0,
+    duration: '200%'
+
+  })
+  .setPin('#slide02 .pin-wrapper')
+  .addTo(controller)
+  ;
+
+  var pinScene03 = new ScrollMagic.Scene({
+    triggerElement: '#slide02',
+    triggerHook: 0,
+    duration: '200%'
+
+  })
+  .setPin('#slide03 .pin-wrapper')
+  .addTo(controller)
+  ;
+
+  var pinScene04 = new ScrollMagic.Scene({
+    triggerElement: '#slide03',
+    triggerHook: 0,
+    duration: '200%'
+
+  })
+  .setPin('#slide04 .pin-wrapper')
+  .addTo(controller)
+  ;
+
+});
