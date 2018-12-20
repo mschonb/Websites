8773420fc9e6bf2fca351c06f48a21cbc35e26e6
mschonb <mschonb@gmail.com>
2018-12-20 12:19:26 -0600

    added StartUp Theme

--- MIQOTEKSIS/js/main.js
+++ MIQOTEKSIS/js/main.js
@@ -1,47 +1,48 @@
 $(document).ready(function(){
 
-  //init ScrollMagic
-  var controller = new ScrollMagic.Controller();
+  if($(window).width() >= 1024){
+    //init ScrollMagic
+    var controller = new ScrollMagic.Controller();
 
-  //Scene 01 - pinned
-  var pinScene01 = new ScrollMagic.Scene({
-    triggerElement: '#slide01',
-    triggerHook: 0,
-    duration: '100%'
+    //Scene 01 - pinned
+    var pinScene01 = new ScrollMagic.Scene({
+      triggerElement: '#slide01',
+      triggerHook: 0,
+      duration: '100%'
 
-  })
-  .setPin('#slide01 .pin-wrapper')
-  .addTo(controller)
-  ;
+    })
+    .setPin('#slide01 .pin-wrapper')
+    .addTo(controller)
+    ;
 
-  var pinScene02 = new ScrollMagic.Scene({
-    triggerElement: '#slide01',
-    triggerHook: 0,
-    duration: '200%'
+    var pinScene02 = new ScrollMagic.Scene({
+      triggerElement: '#slide01',
+      triggerHook: 0,
+      duration: '200%'
 
-  })
-  .setPin('#slide02 .pin-wrapper')
-  .addTo(controller)
-  ;
+    })
+    .setPin('#slide02 .pin-wrapper')
+    .addTo(controller)
+    ;
 
-  var pinScene03 = new ScrollMagic.Scene({
-    triggerElement: '#slide02',
-    triggerHook: 0,
-    duration: '200%'
+    var pinScene03 = new ScrollMagic.Scene({
+      triggerElement: '#slide02',
+      triggerHook: 0,
+      duration: '200%'
 
-  })
-  .setPin('#slide03 .pin-wrapper')
-  .addTo(controller)
-  ;
+    })
+    .setPin('#slide03 .pin-wrapper')
+    .addTo(controller)
+    ;
 
-  var pinScene04 = new ScrollMagic.Scene({
-    triggerElement: '#slide03',
-    triggerHook: 0,
-    duration: '200%'
+    var pinScene04 = new ScrollMagic.Scene({
+      triggerElement: '#slide03',
+      triggerHook: 0,
+      duration: '200%'
 
-  })
-  .setPin('#slide04 .pin-wrapper')
-  .addTo(controller)
-  ;
-
+    })
+    .setPin('#slide04 .pin-wrapper')
+    .addTo(controller)
+    ;
+  }
 });
