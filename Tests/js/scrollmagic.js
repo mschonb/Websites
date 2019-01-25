f4637409b808caba8ad9c0274365a294a7af4fd8
mschonb <mschonb@gmail.com>
2019-01-25 10:34:51 -0600

    improved menu button funcunality

--- Tests/js/scrollmagic.js
+++ Tests/js/scrollmagic.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
 	var pinScene01 = new ScrollMagic.Scene({
 		triggerElement: '#quienes',
 		triggerHook: 0,
-		duration: '100%'
+		duration: '120%'
 	})
 	.setPin('#quienes .pin-wrapper')
 	.addTo(controller)
@@ -16,25 +16,17 @@ $(document).ready(function(){
   var pinscene02 = new ScrollMagic.Scene({
     triggerElement: '#productos',
     triggerHook: 0,
-    duration: '100%'
+    duration: '120%'
   })
   .setPin('#productos .pin-wrapper')
   .addTo(controller)
   ;
 
-  var pinscene03 = new ScrollMagic.Scene({
-    triggerElement: '#equipo',
-    triggerHook: 0,
-    duration: '100%'
-  })
-  .setPin('#equipo .pin-wrapper')
-  .addTo(controller)
-  ;
 
   var pinscene04 = new ScrollMagic.Scene({
     triggerElement: '#contacto',
     triggerHook: 0,
-    duration: '100%'
+    duration: '120%'
   })
   .setPin('#contacto .pin-wrapper')
   .addTo(controller)
@@ -42,21 +34,28 @@ $(document).ready(function(){
 
 	//change manu button color
 
-	var menuColor = new ScrollMagic.Scene({
+	var menuColor01 = new ScrollMagic.Scene({
 		triggerElement: '#quienes',
 		triggerHook: 0,
-		duration: '100%'
+		duration: '120%'
 	})
 	.setClassToggle("#menuicon", "b")
 	.addTo(controller)
 	;
 
-	var menuColor = new ScrollMagic.Scene({
+	var menuColor02 = new ScrollMagic.Scene({
 		triggerElement: '#equipo',
 		triggerHook: 0,
-		duration: '100%'
+		duration: calcHeight()
 	})
 	.setClassToggle("#menuicon", "b")
 	.addTo(controller)
 	;
+
 });
+
+
+function calcHeight(){
+    var relativeheight = String(parseInt(Math.round(document.getElementById('equipo').offsetHeight/$(window).height() * 100)));
+    return relativeheight.concat("%");
+}
